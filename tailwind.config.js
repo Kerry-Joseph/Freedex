3ddeb3926d66c49b1db9624426988c26da81861e
robobait <kerrykj8@yahoo.com>
2022-10-10 12:16:20 -0700

    wip: set theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+      'FGI_white': '#f4ebd9',
+      'FGI_blue' : '#44ccff',
+      'FGI_dark_blue' : '#1b4079',
+    },
     extend: {},
   },
   plugins: [],
